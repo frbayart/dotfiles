12771bfa354cfe6b5809a2de6c2d18ad749b2c22
Francois BAYART <francois@famipow.com>
2023-05-15 09:55:50 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,5 @@
-git config --global gpg.format ssh && git config --global user.signingkey "$SSH_SIGNING_KEY" && git config commit.gpgsign true --global
+git config --global gpg.format ssh
+git config --global user.name "GIT_AUTHOR_NAME"
+git config --global user.email "GIT_AUTHOR_EMAIL"
+git config --global user.signingkey "$SSH_SIGNING_KEY"
+git config commit.gpgsign true --global
