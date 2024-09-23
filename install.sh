730d88711c09551c5229db285b972a4366718be2
Francois Bayart <francois@famipow.com>
2024-09-24 00:10:24 +0200

    update  gitconfig

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
-git config --global gpg.format ssh
-git config --global user.name "GIT_AUTHOR_NAME"
-git config --global user.email "GIT_AUTHOR_EMAIL"
-git config --global user.signingkey "$SSH_SIGNING_KEY"
-git config commit.gpgsign true --global
+#! /usr/bin/env bash
+
+cp .gitignore_global ~/.gitignore_global
+cp .gitconfig ~/.gitconfig
+cp .gitmessage ~/.gitmessage
+
+# git config --global gpg.format ssh
+# git config --global user.name "GIT_AUTHOR_NAME"
+# git config --global user.email "GIT_AUTHOR_EMAIL"
+# git config --global user.signingkey "$SSH_SIGNING_KEY"
+# git config commit.gpgsign true --global
